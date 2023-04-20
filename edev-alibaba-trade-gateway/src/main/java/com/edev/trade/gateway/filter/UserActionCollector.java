e80f7bcec5d2736bee113169de3e98efdcbbd175
Fangang <fangang2004@126.com>
2023-04-20 10:51:03 +0800

    add the user action collect function

--- edev-alibaba-trade-gateway/src/main/java/com/edev/trade/gateway/filter/UserActionCollector.java
+++ edev-alibaba-trade-gateway/src/main/java/com/edev/trade/gateway/filter/UserActionCollector.java
@@ -1,31 +1,30 @@
 package com.edev.trade.gateway.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Component
-public class TokenFilter implements GlobalFilter, Ordered {
+import java.net.URI;
 
+@Component
+@Slf4j
+public class UserActionCollector implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
+        String user = "user";
         String token = request.getHeaders().getFirst("token");
-        if(token==null||token.equals("")) return forbidden(exchange);
-        //valid whether the token has authority.
+        String ip = (request.getRemoteAddress()==null)?"":request.getRemoteAddress().getHostName();
+        HttpMethod method = request.getMethod();
+        URI uri = request.getURI();
+        log.info("{},{},{},{},{}", user, token, ip, method, uri);
         return chain.filter(exchange);
-    }
-
-    private Mono<Void> forbidden(ServerWebExchange exchange) {
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(HttpStatus.FORBIDDEN);
-        return response.setComplete();
     }
 
     @Override
