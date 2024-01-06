4bec3e29d966574a99164aef35d2af4e36693d7b
Fangang <fangang2004@126.com>
2024-01-06 13:30:34 +0800

    add some new functions

---
+++ edev-alibaba-trade-product/src/main/java/com/edev/trade/product/service/ClassifyService.java
@@ -0,0 +1,9 @@
+package com.edev.trade.product.service;
+
+import com.edev.trade.product.entity.Classify;
+
+import java.util.Collection;
+
+public interface ClassifyService {
+    Collection<Classify> getAll();
+}
